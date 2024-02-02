ba2bd73194da8a71c610e78d76f83f5531551618
Tibor <112723857+Tibor0106@users.noreply.github.com>
2024-02-02 14:24:57 +0100

    Home and navbar

---
+++ src/Elements/Navbar.jsx
@@ -0,0 +1,99 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import "./../style.css";
+import loginPng from "./../media/login.png";
+import { useNavigate } from "react-router-dom";
+import { useLocation, Link } from "react-router-dom";
+import { FaEnvelope } from "react-icons/fa";
+import { FaBell } from "react-icons/fa";
+
+function Home() {
+  class NavLink {
+    constructor(id, name, path, pageTitle) {
+      this.id = id;
+      this.name = name;
+      this.path = path;
+      this.pageTitle = pageTitle;
+    }
+  }
+  const location = useLocation();
+  const navigator = useNavigate();
+  const navLink = [
+    new NavLink(1, "Home", "/", "GameHub Főoldal"),
+    new NavLink(6, "rólunk", "/about", "GameHub  Rólunk"),
+    new NavLink(6, "Könyvtár", "/konyvtar", "GameHub Könyvtár"),
+    new NavLink(6, "barátok", "/78", "GameHub Barárok"),
+    new NavLink(6, "Jatékok", "/78", "GameHub Játékok"),
+  ];
+  return (
+    <>
+      <div className="nav">
+        <div className="container">
+          <div className="login_info d-flex justify-content-between mt-3">
+            <p className="email">
+              <FaEnvelope className="me-2" />
+              info@gamehub.hu
+            </p>
+            <p className="login">
+              <img src={loginPng} alt="" className="me-2" />
+              Bejelentkezés
+            </p>
+          </div>
+        </div>
+      </div>
+      <nav className="navbar navbar-expand-lg ">
+        <div className="container">
+          <a className="navbar-brand" href="#">
+            Navbar
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <div className="">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                {navLink.map((i) => (
+                  <li className="nav-item">
+                    <Link
+                      className={`nav-link  ${
+                        location.pathname == i.path ? "navActive" : ""
+                      }`}
+                      to={i.path}>
+                      {i.name}
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            <div className="ms-auto d-flex">
+            
+              <div className="dropdown">
+                <button
+                  className="btn  dropdown-toggle"
+                  type="button"
+                  data-bs-toggle="dropdown"
+                  aria-expanded="false">
+                   <FaBell size={22}  className=""/>
+                   <span className="bellcount">9+</span>
+                </button>
+                <div className="dropdown-menu">
+
+                </div>
+               
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
+    </>
+  );
+}
+export default Home;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./../style.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import Navbar from "./../Elements/Navbar";
+import kep from "./../media/s1.png";
+function Home() {
+  return (
+    <>
+      <Navbar />
+    
+    </>
+  );
+}
+export default Home;
